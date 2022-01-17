8e443dafb2046616462812aff497b8cec2a552e8
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2022-01-17 20:41:27 +0100

    init

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+using ident.Client;
+using ident.Client.Auth;
+using ident.Client.Handler;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// HTTP client configuration
+builder.Services.AddTransient<AntiforgeryHandler>();
+
+builder.Services.AddHttpClient("backend", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<AntiforgeryHandler>();
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("backend"));
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentAuthenticationStateProvider>();
+
+await builder.Build().RunAsync();
